e45c1fc3deb1faef602bfda88a93efbcf746bae0
Wader Hajba <wader.hajba@brunata-hamburg.de>
2021-11-30 16:24:49 +0100

    Große und kleine Buchstaben prüfen, Test Methode

--- java/src/passwortgenerator/PasswortValidation.java
+++ java/src/passwortgenerator/PasswortValidation.java
@@ -1,10 +1,13 @@
 package passwortgenerator;
 
+import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class PasswortValidation {
+
+
     private String passwort;
     private static final int MAX_LENGTH = 10;
     private BufferedReader reader = new BufferedReader(
@@ -12,10 +15,31 @@ public class PasswortValidation {
 
 
     public void passwortEingeben() throws IOException {
-        System.out.println("Passwort Bitte eingeben");
-        String input = reader.readLine();
-        laengeTest(input);
-        nummberTest(input);
+        ArrayList<String> passwoerter = new ArrayList<>();
+        String input = "";
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Passwort Bitte eingeben");
+            input = reader.readLine();
+            if (laengeTest(input) && nummberTest(input) && grossKleinTest(input)) {
+                passwoerter.add(input);
+            }
+        }
+        System.out.println(passwoerter);
+    }
+
+    public ArrayList<String> passwortArray() {
+        ArrayList<String> passwoerterArray = new ArrayList<>();
+        passwoerterArray.add("wader4%");
+        passwoerterArray.add("abc3§");
+        passwoerterArray.add("qwwe$7");
+        passwoerterArray.add("zhjk");
+        for (int i = 0; i < passwoerterArray.size(); i++) {
+            if (!laengeTest(passwoerterArray.get(i)) || !nummberTest(passwoerterArray.get(i))) {
+                System.out.println(passwoerterArray.get(i));
+                return passwoerterArray;
+            }
+        }
+        return null;
     }
 
 
@@ -27,25 +51,39 @@ public class PasswortValidation {
         }
 
         return true;
-
     }
 
     public boolean nummberTest(String passwort) {
 
         if (passwort.matches("\\w+\\W.*")) {
-            System.out.println("das Passwort enthalt Sonderzeichen und Zahlen");
             return true;
-        }
-        else {
+        } else {
             System.out.println("das Passwort enthalt keine Sonderzeichen oder Zahlen ");
             return false;
         }
 
     }
 
+    public boolean grossKleinTest(String passwort) {
+        char ch;
+        boolean capitalFlag = false;
+        boolean lowerCaseFlag = false;
+        for (int i = 0; i < passwort.length(); i++) {
+            ch = passwort.charAt(i);
+            if (Character.isUpperCase(ch)) {
+                capitalFlag = true;
 
+            }
+            if (Character.isLowerCase(ch)) {
+                lowerCaseFlag = true;
 
-
+            }
+            if(capitalFlag && lowerCaseFlag){
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 }
--- java/src/passwortgenerator/PasswortValidationTest.java
+++ java/src/passwortgenerator/PasswortValidationTest.java
@@ -1,30 +1,39 @@
 package passwortgenerator;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 
 class PasswortValidationTest {
 
 
     private PasswortValidation passwordVal = new PasswortValidation();
+
     @Test
-    public void laengeTest(){
-     String passwort = "wader123456";
+    public void laengeTest() {
+        String passwort = "wader12345";
+        assertTrue(passwordVal.laengeTest(passwort));
 
-     assertFalse(passwordVal.laengeTest(passwort));
-
-     passwort = "wader12345";
-     assertTrue(passwordVal.laengeTest(passwort));
+        String passwort2 = "wader123456";
+        assertFalse(passwordVal.laengeTest(passwort2));
     }
 
     @Test
-    public void nummberTest(){
+    public void nummberTest() {
         String passwort = "wader12%";
-        String passwort2  = "rrfgfg";
         assertTrue(passwordVal.nummberTest(passwort));
+
+        String passwort2 = "rrfgfg";
         assertFalse(passwordVal.nummberTest(passwort2));
     }
 
+    @Test
+    public void grossKleinTest() {
+        String passwort = "erT2$";
+        assertTrue(passwordVal.grossKleinTest(passwort));
+
+        String passwort2 = "er2$";
+        assertFalse(passwordVal.grossKleinTest(passwort2));
+    }
 }
